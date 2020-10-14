0a78a47c554a4c302e151ffadd35277115c3d06e
PresentKim <debe3721@gmail.com>
2020-10-14 15:41:51 +0900

    feat : Register "void" chunk generator

---
+++ src/kim/present/generator/void/Loader.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ *
+ *  ____  _             _         _____
+ * | __ )| |_   _  __ _(_)_ __   |_   _|__  __ _ _ __ ___
+ * |  _ \| | | | |/ _` | | '_ \    | |/ _ \/ _` | '_ ` _ \
+ * | |_) | | |_| | (_| | | | | |   | |  __/ (_| | | | | | |
+ * |____/|_|\__,_|\__, |_|_| |_|   |_|\___|\__,_|_| |_| |_|
+ *                |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author  Blugin team
+ * @link    https://github.com/Blugin
+ * @license https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ */
+
+declare(strict_types=1);
+
+namespace kim\present\generator\void;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\world\generator\GeneratorManager;
+
+class Loader extends PluginBase{
+    public function onLoad() : void{
+        GeneratorManager::getInstance()->addGenerator(VoidGenerator::class, "void", true);
+    }
+}
